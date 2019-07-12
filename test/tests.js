ec8ce811f799aa453377a3bb55cc30af0700dd91
allisonbusse <allisonbusse@gmail.com>
2019-07-11 19:05:14 -0700

    working add function

--- test/tests.js
+++ test/tests.js
@@ -1,13 +1,19 @@
+import add from '../add.js';
+
 const test = QUnit.test;
 
-test('time to test!', function(assert) {
+test('add two number', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const x = 3;
+    const y = 4;
+    const expected = 7;
 
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const sum = add(x, y);
+    console.log('hi');
     //Assert
     // What should the result be?
-    assert.equal(true, false);
+    assert.equal(sum, expected);
 });
